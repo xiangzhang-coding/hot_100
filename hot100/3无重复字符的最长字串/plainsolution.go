93342739ffe9e0117835baf8c61a69b7ef9f08a9
seanchueng <hmzhangxiang@gmail.com>
2025-03-11 23:29:52 +0800

    2025-3-11

--- hot100/3无重复字符的最长字串/plainsolution.go
+++ hot100/3无重复字符的最长字串/plainsolution.go
@@ -1,7 +1,10 @@
 func lengthOfLongestSubstring(s string) int {
+	// res is the length of current substring with no repeating characters
 	var res int = 0
+	// maxValue is the maximum length of substring with no repeating characters
 	var maxValue int = 0
 	length := len(s)
+	// O(n) space complexity to store the characters in another formation
 	runes := []rune(s)
 	if length == 1 {
 		return 1
@@ -10,6 +13,8 @@ func lengthOfLongestSubstring(s string) int {
 		return 0
 	}
 	for index, _ := range runes {
+		// O(n^2) space complexity to store the characters in a map
+		// use a map to store that if the character is already in the substring
 		charMap := make(map[rune]bool)
 		for j := index; j < length; j++ {
 
